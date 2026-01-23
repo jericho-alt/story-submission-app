1c07435377fb1868c5ad95a57e7adff6250ce001
jericho-alt <78802739+jericho-alt@users.noreply.github.com>
2026-01-24 01:49:17 +0800

    modified:   frontend/story-app/angular.json modified:   frontend/story-app/src/app/page/admin/admin.component.ts

--- frontend/story-app/src/app/page/admin/admin.component.ts
+++ frontend/story-app/src/app/page/admin/admin.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { StoryListComponent } from '../../components/story-list/story-list.component';
 
 @Component({
-  selector: 'app-admin',
-  imports: [],
-  templateUrl: './admin.component.html',
+  selector: 'app-home',
+  standalone: true,
+  imports: [StoryListComponent],
   styleUrl: './admin.component.css'
 })
 export class AdminComponent {
